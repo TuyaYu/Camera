baa2707af56ea5181021779940f91f0b3f475673
TuyaYu <yuzeyu_jz@163.com>
2017-09-01 11:48:58 +0800

    camera

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fromPhotograph(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera){
+            //创建图片控制器
+            let picker = UIImagePickerController()
+            //设置代理
+            picker.delegate = self
+            //设置来源
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            //允许编辑
+            picker.allowsEditing = true
+            //打开相机
+            self.presentViewController(picker, animated: true, completion: { () -> Void in
+                
+            })
+        }else{
+            print("找不到相机")
+        }
+    }
 
 }
 
